71bc019af10258c2ec40f836405a985d8561ad01
yasiramus <yasirayusif@gmail.com>
2024-10-10 08:44:49 +0000

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'next-monorepo-app',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/next-monorepo-app',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
